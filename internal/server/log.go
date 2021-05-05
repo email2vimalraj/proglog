bbcd4f4f21e4588268ed01629f06c178c81c8aec
Vimalraj Selvam <email2vimalraj@gmail.com>
2021-05-05 09:14:45 +0530

    Commit log prototype

---
+++ internal/server/log.go
@@ -0,0 +1,46 @@
+package server
+
+import (
+	"fmt"
+	"sync"
+)
+
+type Log struct {
+	mu      sync.Mutex
+	records []Record
+}
+
+type Record struct {
+	Value  []byte `json:"value"`
+	Offset uint64 `json:"offset"`
+}
+
+func NewLog() *Log {
+	return &Log{}
+}
+
+// Append appends the log record to the slice and returns the offset (index) of the record
+func (c *Log) Append(record Record) (uint64, error) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	record.Offset = uint64(len(c.records))
+	c.records = append(c.records, record)
+
+	return record.Offset, nil
+}
+
+// Read reads the log record from the given offset.
+// If the offset doesn't exist, returns error.
+func (c *Log) Read(offset uint64) (Record, error) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	if offset >= uint64(len(c.records)) {
+		return Record{}, ErrOffsetNotFound
+	}
+
+	return c.records[offset], nil
+}
+
+var ErrOffsetNotFound = fmt.Errorf("offset not found")
